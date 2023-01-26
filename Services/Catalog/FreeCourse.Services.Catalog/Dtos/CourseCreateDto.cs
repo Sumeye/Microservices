095790c7df7122ebfff7a39f8379dbf6f3e4327e
sumeyye <sumeyye@DESKTOP-77TF0OH>
2023-01-26 03:08:17 +0300

    category services ,course services created

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    internal class CourseCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string picture { get; set; }
+        public string UserId { get; set; }
+        public FeatureDto Feature { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Course
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
